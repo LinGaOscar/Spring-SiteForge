6f45b056b109ea5e2232323c70ae0b752c7e3310
LinGaOscar <oscar987123@yahoo.com.tw>
2026-05-24 17:52:43 +0800

    feat(web): add RWD header/footer templates with Bootstrap 5, add HEADER_MINIMAL and FOOTER_MINIMAL to TemplateKey

--- portal-domain/src/main/java/com/siteforge/domain/enums/TemplateKey.java
+++ portal-domain/src/main/java/com/siteforge/domain/enums/TemplateKey.java
@@ -2,7 +2,9 @@ package com.siteforge.domain.enums;
 
 public enum TemplateKey {
     HEADER_DEFAULT,
+    HEADER_MINIMAL,
     FOOTER_DEFAULT,
+    FOOTER_MINIMAL,
     BODY_STANDARD,
     BODY_LANDING
 }
